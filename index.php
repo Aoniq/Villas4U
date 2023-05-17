6cd337e502433b771e088d17737d00542e28c161
jeffrey.vanoordvandervlies@gmail.com <jeffrey.vanoordvandervlies@gmail.com>
2023-05-17 16:25:13 +0200

    Index pagina
    To do:
    footer logos centeren
    
    Co-Authored-By: lamineja <122975724+lamineja@users.noreply.github.com>

---
+++ index.php
@@ -0,0 +1,160 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>Luxury</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS v5.2.1 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <script src="https://kit.fontawesome.com/23451d210d.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+  <!-- place header here -->
+  <header class="header">
+    <div class="container-fluid">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+        <img src="./assets/img/logo.png" alt="" height = "110" class="">
+        </a>
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+          <li><a href="#" class="nav-link px-2 text-black fw-bold">Home</a></li>
+          <li><a href="#" class="nav-link px-2 text-black fw-bold">Villa's</a></li>
+          <li><a href="#" class="nav-link px-2 text-black fw-bold">Contact</a></li>
+        </ul>
+
+        <div class="text-end">
+          <button type="button" class= "px-4 py-2 btn btn-outline-warning text-bright border-warning border-5 rounded-4 shadow">Login</button>
+          <button type="button" class="px-4 py-2 btn btn-warning text-white rounded-4 shadow">Registeren</button>
+        </div>
+      </div>
+    </div>
+ 
+  </header>
+  <main>
+    <div class="container mt-5">
+        <div class="row justify-content-center text-center items-center">
+            <h1 class="fw-bolder">Villa te koop</h1>
+            <h5>Where luxury needs exclusivity</h5>
+        </div>
+        <div class="row gy-5">
+            <div class="col-md-4">
+                <div class="card p-0 shadow">
+                    <img src="./assets/img/villa1-banner.png" alt="" class="card-img-top">
+                    <div class="card-body">
+                        <ul class="list-unstyled">
+                            <li class="fs-5 fw-semibold"><i class="fa-solid fa-check"></i> 100m2 Achtertuin</li>
+                            <li class="fs-5 fw-semibold"><i class="fa-solid fa-check"></i> 5 Slaapkamers</li>
+                            <li class="fs-5 fw-semibold"><i class="fa-solid fa-check"></i> Binnen garage</li>
+                        </ul>
+                    </div>
+                    <a href="#" class="btn btn-warning w-100">Bekijk meer</a>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card p-0 shadow">
+                    <img src="./assets/img/villa2-banner.png" alt="" class="card-img-top">
+                    <div class="card-body">
+                        <ul class="list-unstyled">
+                            <li class="fs-5 fw-semibold"><i class="fa-solid fa-check"></i> 100m2 Achtertuin</li>
+                            <li class="fs-5 fw-semibold"><i class="fa-solid fa-check"></i> 5 Slaapkamers</li>
+                            <li class="fs-5 fw-semibold"><i class="fa-solid fa-check"></i> Binnen garage</li>
+                        </ul>
+                    </div>
+                    <a href="#" class="btn btn-warning w-100">Bekijk meer</a>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card p-0 shadow">
+                    <img src="./assets/img/villa3-banner.png" alt="" class="card-img-top">
+                    <div class="card-body">
+                        <ul class="list-unstyled">
+                            <li class="fs-5 fw-semibold"><i class="fa-solid fa-check"></i> 100m2 Achtertuin</li>
+                            <li class="fs-5 fw-semibold"><i class="fa-solid fa-check"></i> 5 Slaapkamers</li>
+                            <li class="fs-5 fw-semibold"><i class="fa-solid fa-check"></i> Binnen garage</li>
+                        </ul>
+                    </div>
+                    <a href="#" class="btn btn-warning w-100">Bekijk meer</a>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card p-0 shadow">
+                    <img src="./assets/img/villa4-banner.png" alt="" class="card-img-top">
+                    <div class="card-body">
+                        <ul class="list-unstyled">
+                            <li class="fs-5 fw-semibold"><i class="fa-solid fa-check"></i> 100m2 Achtertuin</li>
+                            <li class="fs-5 fw-semibold"><i class="fa-solid fa-check"></i> 5 Slaapkamers</li>
+                            <li class="fs-5 fw-semibold"><i class="fa-solid fa-check"></i> Binnen garage</li>
+                        </ul>
+                    </div>
+                    <a href="#" class="btn btn-warning w-100">Bekijk meer</a>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card p-0 shadow">
+                    <img src="./assets/img/villa5-banner.png" alt="" class="card-img-top">
+                    <div class="card-body">
+                        <ul class="list-unstyled">
+                            <li class="fs-5 fw-semibold"><i class="fa-solid fa-check"></i> 100m2 Achtertuin</li>
+                            <li class="fs-5 fw-semibold"><i class="fa-solid fa-check"></i> 5 Slaapkamers</li>
+                            <li class="fs-5 fw-semibold"><i class="fa-solid fa-check"></i> Binnen garage</li>
+                        </ul>
+                    </div>
+                    <a href="#" class="btn btn-warning w-100">Bekijk meer</a>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card p-0 shadow">
+                    <img src="./assets/img/villa6-banner.png" alt="" class="card-img-top">
+                    <div class="card-body">
+                        <ul class="list-unstyled">
+                            <li class="fs-5 fw-semibold"><i class="fa-solid fa-check"></i> 100m2 Achtertuin</li>
+                            <li class="fs-5 fw-semibold"><i class="fa-solid fa-check"></i> 5 Slaapkamers</li>
+                            <li class="fs-5 fw-semibold"><i class="fa-solid fa-check"></i> Binnen garage</li>
+                        </ul>
+                    </div>
+                    <a href="#" class="btn btn-warning w-100">Bekijk meer</a>
+                </div>
+            </div>
+        </div>
+    </div>
+  </main>
+    <!-- place footer here -->
+  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 mt-4 footer">
+  <ul class="nav col-md-4 justify-content-start">
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Mobile App</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Community</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Company</a></li>
+    </ul>
+
+    <a href="#" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+        <img src="./assets/img/logo.png" class="img-fluid" alt="">
+    </a>
+
+    <ul class="nav col-md-4 justify-content-end">
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Help Desk</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Blog</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Resources</a></li>
+    </ul>
+    <div class="row justify-content-between align-items-center w-100">
+        <div class="col-4"><i class="fa-brands fa-github"></i></div>
+        <div class="col-4"><i class="fa-brands fa-instagram"></i></div>
+        <div class="col-4"><i class="fa-brands fa-facebook"></i></div>
+    </div>
+  </footer>
+  <!-- Bootstrap JavaScript Libraries -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+  </script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
+    integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+  </script>
+</body>
+
+</html>
